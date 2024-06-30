ef13e2ea310f61b007f4a8bce7695996555a9f4e
Haco <haco.tw@gmail.com>
2024-07-01 01:57:43 +0800

    fix: bug

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5001;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hello', (req, res) => {
-    console.log('Extension Connected!');
+app.get('/', (req, res) => {
     res.send('Hello World!');
-});
+})
 
 app.post('/', (req, res) => {
     console.log(`Key: ${req.body.key}`)
     res.send('Thanks for your cool stuff!');
 })
 
+app.get('/hello', (req, res) => {
+    console.log('Extension Connected!');
+    res.send('Hello World!');
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Mingdao Logger Backend Started On Port ${port}`);
 })
